e6217e0d6af8948baaa39d47f240e30f531cf330
MuhammadTaha25 <mtahashahid230@gmail.com>
2025-01-19 18:03:50 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
 import streamlit as st
+from streamlit_mic_recorder import mic_recorder
+from audio_handler import transcribe_audio
 from langchain_community.document_loaders import WebBaseLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
@@ -82,7 +84,17 @@ _chain = setup | _prompt | llm | StrOutputParser()  # Define the complete proces
 st.title("Ask Anything About Elon Musk")
 
 # Create a container for displaying chat history.
+voice_recording_column, send_button_column = st.columns(2)
 chat_container = st.container()
+with voice_recording_column:
+    voice_recording=mic_recorder(start_prompt="Start recording",stop_prompt="Stop recording", just_once=True)
+with send_button_column:
+    send_button = st.button("Send", key="send_button", on_click=clear_input_field)
+
+ if voice_recording:
+        transcribed_audio = transcribe_audio(voice_recording["bytes"])
+        query=print(transcribed_audio)
+
 
 # Initialize session state to keep track of chat history.
 if "messages" not in st.session_state:
@@ -98,7 +110,7 @@ with st.container():
     send_button = st.button("Send", key="send_btn")  # Button to send the query.
 
 # Process the user's query and generate a response.
-if send_button or st.session_state.get("send_input") and query:
+if send_button or st.session_state.get("send_input") and query or voice_recording:
     with st.spinner("Processing... Please wait!"):  # Display a spinner while processing.
         response = _chain.invoke({'question': query})  # Generate the response.
     # Save user query and AI response in chat history.
